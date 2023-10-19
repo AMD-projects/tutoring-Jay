dc3d462a4fde716137ef5a7d1a1dce891801254c
AMD-projects <alex.mcdowell.138@gmail.com>
2023-10-19 11:25:15 +0100

    beginning of object oreinted programming

---
+++ lessons/oop_exercises.py
@@ -0,0 +1,7 @@
+# object oriented programming exercises
+
+# Create a class hierarchy for basic geometry shapes: Shape (abstract base class), Circle, and Rectangle. 
+# Each shape should have attributes for its dimensions and a method to calculate its area. 
+# Use inheritance and abstraction.
+
+        
